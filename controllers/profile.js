4cdd9e564b7f7aa8211d08d10ca357106ddfe1a3
iamaryankr <95643709+iamaryankr@users.noreply.github.com>
2024-06-07 21:59:28 +0530

    backend complete

---
+++ controllers/profile.js
@@ -0,0 +1,15 @@
+const handleProfile = (req, res, db) => {
+    const {id} = req.params;
+    db.select('*').from('users').where({
+        id: id
+    })
+    .then(user => {
+       if(user.length)  res.json(user[0])
+        else res.status(400).json('not found')
+    })
+    .catch(err => res.status(400).json('error gettin user'))
+}
+
+module.exports = {
+    handleProfile: handleProfile
+}
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors'); //to give access to browsers
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const app = express();
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'postgres',
+      password: 'aryan',
+      database: 'smart-brain',
+    },
+});
+
+db.select('*').from('users').then(data => {
+    console.log(data)
+});
+  
 
 app.use(express.json());
 app.use(cors());
@@ -14,92 +36,13 @@ register --> POST = user
 profile/:userId --> GET = user
 image --> PUT --> user
 */
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'john',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
 
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
 
-app.post('/signin', (req, res) => {
-    bcrypt.compare("apples", '$2a$10$Kqaj26PZY95OVsN5qUgnyOVHGj4b8QVef.S1z3wfI/BdL9eivuvlK', function(err, hash){
-        console.log('first guess', res)
-    });
-    bcrypt.compare("veggies", '$2a$10$Kqaj26PZY95OVsN5qUgnyOVHGj4b8QVef.S1z3wfI/BdL9eivuvlK', function(err, hash){
-        console.log('second guess', res)
-    });
-    if(req.body.email === database.users[0].email &&
-       req.body.password === database.users[0].password){
-        res.json(database.users[0]);
-    }
-    else res.status(400).json('error loggin in');
-
-})
-
-app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
-    bcrypt.hash(password, null, null, function(err, hash){
-        console.log(hash);
-    });
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length-1])
-})
-
-app.get('/profile/:id', (req, res) => {
-    const {id} = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            return res.json(user);
-        } 
-    })
-    if(!found) res.status(404).json('no user exists!');
-})
-
-app.put('/image', (req, res) => {
-    const {id} = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        } 
-    })
-    if(!found) res.status(404).json('no user exists!');
-})
+app.get('/', (req, res) => {res.send(database.users)})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
+app.put('/image', (req, res) => {image.handeImage(req, res, db)})
 
 app.listen(3000, () => {
     console.log('app is running!');
