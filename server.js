43e678d88dfcd04f22850a865799a8712adfb699
iamaryankr <95643709+iamaryankr@users.noreply.github.com>
2024-06-09 00:36:41 +0530

    *

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ image --> PUT --> user
 */
 
 
-app.get('/', (req, res) => {res.send('app is working!')})
+app.get('/*', (req, res) => {res.send('app is working!')})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
